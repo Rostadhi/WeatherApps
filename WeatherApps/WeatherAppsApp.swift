b260d3bdcf05f974d611cc2a4d34e5fffb681fb8
Rostadhi <rostadhi@gmail.com>
2022-01-06 19:59:47 +0700

    Initial Commit

---
+++ WeatherApps/WeatherAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppsApp.swift
+//  WeatherApps
+//
+//  Created by Rostadhi Akbar, M.Pd on 06/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
